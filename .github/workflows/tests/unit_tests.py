0d175093148f7b5fb142042c5ac3974e7fe65eb3
SmtOrtakaya <ortakaya46@gmail.com>
2026-01-15 02:53:35 +0300

    Delete .github/workflows directory

--- .github/workflows/tests/unit_tests.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-def get_hash_bucket(input_value, buckets=1000):
-    return abs(hash(input_value)) % buckets
-
-def test_hashing_consistency():
-    test_input = "high_cardinality_feature_1"
-    result = get_hash_bucket(test_input)
-    assert 0 <= result < 1000
